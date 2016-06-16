97a2f62dc26fc9cd7f123abd48e6fb57ce69cef0
나상권 <sknah@nasang-gwon-ui-MacBook-Pro.local>
2016-06-17 00:55:53 +0900

    scrollbar

--- template/js/directives/listbox.js
+++ template/js/directives/listbox.js
@@ -1,7 +1,7 @@
 /* global define */
 'use strict';
 
-define([/*foo/bar*/], function () {
+define(['mCustomScrollbar'], function (mCustomScrollbar) {
 	return ['listbox', function () {
 		return {
 			restrict: 'E',
@@ -10,7 +10,15 @@ define([/*foo/bar*/], function () {
 			scope: {
 				data: '=lData'
 			},
-			controller: ['$scope', function ($scope) {
+			controller: ['$scope','$element','$timeout', function ($scope, $element, $timeout) {
+					$timeout(function(){$element.children('ul').mCustomScrollbar({
+									axis: 'y',
+									theme: 'dark-thin',
+									scrollbarPosition: 'outside',
+									mouseWheel: {
+										enable: true
+									}
+								});});
 			}],
 			link: function (/* scope (can be used.) */) {
 				// $('#example').fadeIn(300);
